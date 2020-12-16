cd15d7c15fe3e51b30f4e721dffdc07f6b6cb090
irohjenny <irohjenny@gmail.com>
2020-12-16 12:15:56 +0100

    added navbar and hero page

---
+++ assets/scripts.js
@@ -0,0 +1,13 @@
+// const showMenu = (sidenav, navId) => {
+//     const sidenav = document.getElementsByClassName(sidenav),
+//         nav = document.getElementById(navId);
+
+//     if (sidenav && nav) {
+//         sidenav.addEventListener("click", () => {
+//             nav.classList.sidenav("show-on-medium-and-down");
+//         });
+//     }
+// };
+
+
+
