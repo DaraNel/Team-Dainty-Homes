204cc7f16edd524b323e643a250cd2f0d8e7e10e
Iroh Jennifer <irohjenny@gmail.com>
2020-12-17 16:32:54 +0100

    added an about and team members section

---
+++ assets/scripts.js
@@ -0,0 +1,16 @@
+
+
+//scrollspy functions
+$(document).ready(function(){
+    //side nav
+    $(".sidenav ").sidenav();
+
+    //scrollspy
+    $('.scrollspy').scrollSpy();
+
+    //parallax
+    $('.parallax').parallax();
+});
+
+
+      
